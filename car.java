f972790f2f11652a1a5c4b5b0116f9a1ef8419a0
Ritesh naik <riteshnaik242008@gmail.com>
2026-03-07 10:03:06 +0530

    java program

---
+++ car.java
@@ -0,0 +1,15 @@
+public class car {
+    void fullThrottle(){
+        System.out.println("car is moving very fast");
+    }
+    void  maxspeed(int speed){
+        System.out.println("max speed is"+speed);
+
+    }
+public static void main(String[] args) {
+    car mycar=new car();
+    mycar.fullThrottle();
+    mycar.maxspeed(160);
+}
+}
+                                                                                                    
